6b7b8cf601975e0bb3f9413df36cf29fd80cf450
Vladimir Vilenskii <vilvl@equai.xyz>
2024-03-11 09:38:57 +0200

    graphics

---
+++ src/graphics/mlx_utils.c
@@ -0,0 +1,28 @@
+#include "mlx_utils.h"
+
+#include "mlx.h"
+#include <stddef.h>
+
+t_image	*init_image(
+	void *mlx, t_image *img, int width, int height)
+{
+	img->image = mlx_new_image(mlx, width, height);
+	if (!img->image)
+		return (NULL);
+	img->addr = mlx_get_data_addr(img->image, &img->bits_per_pixel,
+			&img->line_length, &img->endian);
+	return (img);
+}
+
+void	put_pixel(t_image *img, int x, int y, unsigned int color)
+{
+	char	*dst;
+
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+static unsigned int	pixel(u_int8_t r, u_int8_t g, u_int8_t b, u_int8_t a)
+{
+	return (r << 16 | g << 8 | b | a << 24);
+}
---
+++ src/graphics/mlx_utils.h
@@ -0,0 +1,21 @@
+#ifndef MLX_UTILS_H
+#define MLX_UTILS_H
+
+#include <sys/types.h>
+#include "mlx.h"
+
+typedef struct s_image {
+	void	*image;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}		t_image;
+
+t_image	*init_image(void *mlx, t_image *img, int width, int height);
+
+void	put_pixel(t_image *img, int x, int y, unsigned int color);
+
+static unsigned int	pixel(u_int8_t r, u_int8_t g, u_int8_t b, u_int8_t a);
+
+#endif  // MLX_UTILS_H
