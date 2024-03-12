9b5cc5a3608199b1b6a1e9834d98b41eb5113007
Vladimir Vilenskii <vilenskii.v@gmail.com>
2024-03-12 21:50:54 +0100

    logic-fixes

--- src/hooks/hooks.c
+++ src/hooks/hooks.c
@@ -4,6 +4,7 @@
 
 void	set_hooks(t_app *app)
 {
+	mlx_do_key_autorepeatoff(app->gr.mlx);
 	mlx_hook(app->gr.win, KeyPress, KeyPressMask,
 		key_press_hook, &app);
 	mlx_hook(app->gr.win, KeyRelease, KeyReleaseMask,
