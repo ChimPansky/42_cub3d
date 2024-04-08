7adbe3e4d08cd461a795cc2edb8f4019f9cd88fc
Thomas Kasbarian <tkasbari@c2r5p10.42vienna.com>
2024-04-08 13:13:50 +0200

    moved DBL_TOLERANCE into header file

--- src/utils.h
+++ src/utils.h
@@ -3,6 +3,9 @@
 
 #include <stdbool.h>
 
+// can increase factor of DBL_EPSILON to increase tolerance
+#define DBL_TOLERANCE (DBL_EPSILON * 1)
+
 void	skip_spaces(char **str);
 int		print_error(char *err_msg);
 bool	dbl_is_zero(double number);
