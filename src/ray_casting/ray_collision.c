aba2b2560572d30eb03eb76abfe11f0322180284
Thomas Kasbarian <tkasbari@c3r2p4.42vienna.com>
2024-04-10 16:46:02 +0200

    fixes

--- src/ray_casting/ray_collision.c
+++ src/ray_casting/ray_collision.c
@@ -6,7 +6,7 @@
 /*   By: tkasbari <thomas.kasbarian@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 10:30:58 by tkasbari          #+#    #+#             */
-/*   Updated: 2024/04/09 16:46:25 by tkasbari         ###   ########.fr       */
+/*   Updated: 2024/04/10 16:35:02 by tkasbari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@
 
 static t_collision_direction	get_collision_direction(t_raycaster *rc)
 {
-	if (dbl_is_zero(rc->end_point.x - (int)rc->end_point.x))
+	if (fabs(rc->end_point.x - round(rc->end_point.x))
+		< fabs(rc->end_point.y - round(rc->end_point.y)))
 	{
 		if (rc->map_dir_x > 0)
 			return (COLL_EA);
