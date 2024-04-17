bff20ec81477de6d31cb42d23804a5cc61eccc77
Thomas Kasbarian <tkasbari@c4r1p10.42vienna.com>
2024-04-17 09:32:28 +0200

    adjustment

--- src/utils.c
+++ src/utils.c
@@ -10,17 +10,21 @@ void	skip_spaces(char **str)
 
 int	print_error(char *err_msg)
 {
-	// ft_dprintf(STDERR_FILENO, COLOR_RED);
 	ft_dprintf(STDERR_FILENO, "Error\n");
 	if (err_msg)
 		ft_dprintf(STDERR_FILENO, "%s\n", err_msg);
 	else
 		ft_dprintf(STDERR_FILENO, "Unknown Error\n");
-	// ft_dprintf(STDERR_FILENO, COLOR_RESET);
 	return (FAILURE);
 }
 
 bool	dbl_is_almost_zero(double a)
 {
-	return (a < 10e-10);
+	if (a == 0)
+		return (true);
+	if (a < 0 && a > -10e-10)
+		return (true);
+	if (a > 0 && a < 10e-10)
+		return (true);
+	return (false);
 }
