26d108c02c32cbb0627ac16b627df35e78663aed
Thomas Kasbarian <tkasbari@c4r1p10.42vienna.com>
2024-04-17 09:58:17 +0200

    removed comment from utils.h

--- src/utils.h
+++ src/utils.h
@@ -3,8 +3,6 @@
 
 #include <stdbool.h>
 
-// can increase factor of DBL_EPSILON to increase tolerance
-
 void	skip_spaces(char **str);
 int		print_error(char *err_msg);
 bool	dbl_is_almost_zero(double a);
