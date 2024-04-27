67ebf1510d4f6a6a7c5017956d4d0afb9eafb511
Vladimir Vilenskii <vvilensk@c1r2p10.42vienna.com>
2024-04-27 14:29:54 +0200

    mm-fix

--- src/structs/map.c
+++ src/structs/map.c
@@ -2,8 +2,8 @@
 
 t_map_sym	coord_to_map_sym(t_map *map, t_pos *coord)
 {
-	const int	x = coord->x;
-	const int	y = coord->y;
+	const int	x = coord->x - (coord->x < 0);
+	const int	y = coord->y - (coord->y < 0);
 
 	if (x >= (int)map->width || y >= (int)map->height || x < 0 || y < 0)
 		return (UNKNOWN_SYM);
