2ab6f36217d9e5989e98ff3bc39dec2387b1d126
Thomas Kasbarian <thomas.kasbarian@gmail.com>
2024-04-27 11:52:57 +0200

    save

--- src/structs/sprites.h
+++ src/structs/sprites.h
@@ -3,8 +3,7 @@
 
 # include "image.h"
 
-//#define DOOR_SPRITE_PATH "textures/door.xpm"
-#define DOOR_SPRITE_PATH "sprites/door.xpm"
+#define SPRITE_DOOR_PATH "sprites/door.xpm"
 
 typedef enum	e_sprite_types
 {
