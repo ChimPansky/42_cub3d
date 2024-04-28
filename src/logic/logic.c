36e6eed06cf9c832401ee131b4a6c4aa5242cdc8
Thomas Kasbarian <thomas.kasbarian@gmail.com>
2024-04-28 18:15:46 +0200

    added basic wall collision(buggy)

--- src/logic/logic.c
+++ src/logic/logic.c
@@ -1,5 +1,9 @@
 #include "logic.h"
+#include "structs/ray.h"
 #include <math.h>
+#include <stdbool.h>
+#include "./structs/ray.h"
+#include "./vector/vector.h"
 
 void	process_inputs(t_game_state *game_state, t_inputs *inputs)
 {
@@ -11,12 +15,47 @@ void	process_inputs(t_game_state *game_state, t_inputs *inputs)
 	inputs->mouse_dy = 0;
 }
 
+bool	player_can_move_orth(t_game_state *game)
+{
+	t_ray		move_ray;
+
+	if (game->player.speed.ort > 0)
+		move_ray = ray_init(game->player.pos, game->player.angle + M_PI / 2);
+	else
+		move_ray = ray_init(game->player.pos, game->player.angle - M_PI / 2);
+	calculate_ray_collision(&move_ray, &game->map);
+	if (move_ray.raycaster.collision.distance
+		< PLAYER_SPEED_FIELD_PER_FRAME)
+		return (false);
+	return (true);
+}
+
+
+bool	player_can_move_forw(t_game_state *game)
+{
+	t_ray		move_ray;
+
+	if (game->player.speed.forw > 0)
+		move_ray = ray_init(game->player.pos, game->player.angle);
+	else
+		move_ray = ray_init(game->player.pos, game->player.angle + M_PI);
+	calculate_ray_collision(&move_ray, &game->map);
+	if (move_ray.raycaster.collision.distance
+		< PLAYER_SPEED_FIELD_PER_FRAME)
+		return (false);
+	return (true);
+}
+
 void	change_state_for_next_frame(t_game_state *game_state)
 {
 	t_player	*player;
 
 	player = &game_state->player;
 	player->angle += player->rot_speed * PLAYER_RAD_PER_FRAME;
+	if (!dbl_is_almost_zero(player->speed.forw))
+		player->speed.forw *= player_can_move_forw(game_state);
+	if (!dbl_is_almost_zero(player->speed.ort))
+		player->speed.ort *= player_can_move_orth(game_state);
 	update_coords(&player->pos, &player->speed, player->angle,
 		PLAYER_SPEED_FIELD_PER_FRAME);
 }
